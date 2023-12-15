1d2147c83ad4fe43b815dd11a6360ff7ce465e62
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2023-12-15 09:04:25 +0100

    structure

---
+++ assignment/03 TestQueryTrigger.sql
@@ -0,0 +1,14 @@
+-- test error in insert trigger on support
+
+INSERT into SUPPORT (ConfigID, FehlerKategorieID, ErfasserID, PersonalbereichID)
+VALUES (
+    2, 
+    2, 
+    1001,
+    1
+)
+
+
+
+
+
